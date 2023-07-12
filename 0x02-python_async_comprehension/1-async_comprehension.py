5a68769b06c9bbd1e5e7edc5f070d6e1b612b084
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-07-12 08:03:49 +0000

    changes

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,14 +2,10 @@
 
 """ Async Comprehensions """
 
-from asyncio import sleep
-from random import uniform
-from typing import List
+import asyncio
+import random
+from 0-async_generator.py import async_generator
 
-async_generator = __import__('0-async_generator').async_generator
-
-
-async def async_comprehension() -> List[float]:
-    """ Async Comprehensions  """
-    random_list = [i async for i in async_generator()]
-    return random_list
+async def async_comprehension():
+    random_numbers = [number async for number in async_generator()]
+    return random_numbers
